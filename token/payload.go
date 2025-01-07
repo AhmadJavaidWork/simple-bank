cc77c7077c181959766e467c200ee01f8ceb4420
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2025-01-07 15:13:59 +0500

    added jwt token maker

---
+++ token/payload.go
@@ -0,0 +1,74 @@
+package token
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+var (
+	ErrInvalidToken = errors.New("token is invalid")
+	ErrExpiredToken = errors.New("token has expired")
+)
+
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+// GetAudience implements jwt.Claims.
+func (p *Payload) GetAudience() (jwt.ClaimStrings, error) {
+	return nil, nil
+}
+
+// GetExpirationTime implements jwt.Claims.
+func (p *Payload) GetExpirationTime() (*jwt.NumericDate, error) {
+	return jwt.NewNumericDate(p.ExpiredAt.UTC()), nil
+}
+
+// GetIssuedAt implements jwt.Claims.
+func (p *Payload) GetIssuedAt() (*jwt.NumericDate, error) {
+	return jwt.NewNumericDate(p.IssuedAt.UTC()), nil
+}
+
+// GetIssuer implements jwt.Claims.
+func (p *Payload) GetIssuer() (string, error) {
+	return "simple-bank", nil
+}
+
+// GetNotBefore implements jwt.Claims.
+func (p *Payload) GetNotBefore() (*jwt.NumericDate, error) {
+	return nil, nil
+}
+
+// GetSubject implements jwt.Claims.
+func (p *Payload) GetSubject() (string, error) {
+	return p.Username, nil
+}
+
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+
+	return payload, nil
+}
+
+func (payload *Payload) Valid() error {
+	if time.Now().After(payload.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
