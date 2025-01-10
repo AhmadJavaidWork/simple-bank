5de9fda008e9245afc6b5a8a141113c217395fb7
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2025-01-10 13:36:12 +0500

    added validations for login user

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -7,6 +7,8 @@ import (
 	db "github.com/ahmadjavaidwork/simple-bank/db/sqlc"
 	"github.com/ahmadjavaidwork/simple-bank/pb"
 	"github.com/ahmadjavaidwork/simple-bank/util"
+	"github.com/ahmadjavaidwork/simple-bank/val"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -16,6 +18,11 @@ func (server *Server) LoginUser(
 	ctx context.Context,
 	req *pb.LoginUserRequest,
 ) (*pb.LoginUserResponse, error) {
+	violations := validateLoginUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
 	user, err := server.store.GetUser(ctx, req.GetUsername())
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -68,4 +75,18 @@ func (server *Server) LoginUser(
 		RefreshTokenExpiresAt: timestamppb.New(refreshPayload.ExpiredAt),
 	}
 	return rsp, nil
+}
+
+func validateLoginUserRequest(
+	req *pb.LoginUserRequest,
+) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+
+	return violations
 }
